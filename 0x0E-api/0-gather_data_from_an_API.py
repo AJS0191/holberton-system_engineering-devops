ffc6599af6d0db55c0cc54452b5d1ac8f64f2760
AJS9091 <ashtonjamess@live.com>
2022-03-25 11:02:38 -0700

    pycodestyle:

--- 0x0E-api/0-gather_data_from_an_API.py
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -5,20 +5,21 @@ import json
 import requests
 from sys import argv
 
+
 def main():
     """grabs and displays info from a given API1"""
-    #employees == users && tasks == todos
+    # employees == users && tasks == todos
     apiResponse = requests.get(
         'https://jsonplaceholder.typicode.com/users'
         )
 
-    #print(apiResponse.json())
+    # print(apiResponse.json())
     userResponseList = apiResponse.json()
     for dic in userResponseList:
-        #print(f'this is type of dict id: {type(dic["id"])}')
-        #print(f'this is type of argv[1]: {type(argv[1])}')
+        # print(f'this is type of dict id: {type(dic["id"])}')
+        # print(f'this is type of argv[1]: {type(argv[1])}')
         if dic["id"] == int(argv[1]):
-            #print(f'Got a match {dic["id"]} {argv[1]}')
+            # print(f'Got a match {dic["id"]} {argv[1]}')
             employeeDict = dic
             break
     allTasks = []
@@ -29,16 +30,16 @@ def main():
     taskResponseList = taskResponse.json()
     for dict in taskResponseList:
         if dict['userId'] == int(argv[1]):
-            #print('got a match')
+            # print('got a match')
             allTasks.append(dict)
     for task in allTasks:
-        if task['completed'] == True:
+        if task['completed'] is True:
             completed.append(task)
     print('Employee {} is done with tasks({}/{}):'.format(employeeDict['name'],
                                                           len(completed),
                                                           len(allTasks)))
     for task in completed:
-          print('\t {}'.format(task['title']))
+        print('\t {}'.format(task['title']))
 
 
 if __name__ == '__main__':
--- 0x0E-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x0E-api/3-dictionary_of_list_of_dictionaries.py
@@ -6,44 +6,43 @@ import os
 import requests
 from sys import argv
 
+
 def main():
     """grabs and displays info from a given API1"""
-    #employees == users && tasks == todos
+    # employees == users && tasks == todos
     apiResponse = requests.get(
         'https://jsonplaceholder.typicode.com/users'
         )
     n = 1
-    #print(apiResponse.json())
+    # print(apiResponse.json())
     userResponseList = apiResponse.json()
     for dic in userResponseList:
         if n == 1:
-            employeeDict = {n:dic}
+            employeeDict = {n: dic}
         else:
-            employeeDict.update({n:dic})
+            employeeDict.update({n: dic})
         n += 1
-    #print(employeeDict)
+    # print(employeeDict)
     allTasks = []
     taskResponse = requests.get(
         'https://jsonplaceholder.typicode.com/todos'
         )
     taskResponseList = taskResponse.json()
-    #print(f'-------------------------------------{taskResponseList}----------------------------------')
     for dict in taskResponseList:
-            allTasks.append(dict)
+        allTasks.append(dict)
 
     jsonDic = {}
-    
     for iD, employee in employeeDict.items():
         tempList = []
         for task in allTasks:
             if task['userId'] == iD:
-                tempDic = {"username":employee['username'],
-                           'task':task['title'],
-                           "completed":task['completed']
-                }
+                tempDic = {"username": employee['username'],
+                           'task': task['title'],
+                           "completed": task['completed']
+                           }
                 tempList.append(tempDic)
         jsonDic.update({iD: tempList})
-    
+
     with open('todo_all_employees.json', 'w', encoding='utf-8') as jsonFile:
         jString = json.dumps(jsonDic)
         jsonFile.write(jString)
