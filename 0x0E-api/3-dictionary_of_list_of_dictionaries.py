b39274ab4a1fd21266309e96894504f910193958
AJS9091 <ashtonjamess@live.com>
2022-03-24 22:49:07 -0700

    task 3 works

--- 0x0E-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x0E-api/3-dictionary_of_list_of_dictionaries.py
@@ -19,38 +19,39 @@ def main():
         if n == 1:
             employeeDict = {n:dic}
         else:
-            employeeDict[{n:dic}]
+            employeeDict.update({n:dic})
         n += 1
-    print(employeeDict)
-    allTasks = []
-    completed = []
+    #print(employeeDict)
+    allTasks = {}
     taskResponse = requests.get(
         'https://jsonplaceholder.typicode.com/todos'
         )
     taskResponseList = taskResponse.json()
+    #print(f'-------------------------------------{taskResponseList}----------------------------------')
     for dict in taskResponseList:
-        if dict['userId'] == int(argv[1]):
             #print('got a match')
-            allTasks.append(dict)
-    for task in allTasks:
-        if task['completed'] == True:
-            completed.append(task)
+            allTasks.update({dict['userId']:dict})
 
     attrList = []
-    n = 1
-'''
-for task in allTasks:
-        tempDic = {"username":
+    newAttrList = []
+
+    for userid, task in allTasks.items():
+        tempDic = {"username":employeeDict[userid]['username'],
                    'task':task['title'],
                    "completed":task['completed']
         }
         attrList.append(tempDic)
-    #print(attrList)
-    jsonDic = {argv[1]:attrList}
-    with open('{}.json'.format(argv[1]), 'w', encoding='utf-8') as jsonFile:
+    efnDic = {}
+    for iD, employee in employeeDict.items():
+        for task in attrList:
+            if employee['username'] == task['username']:
+                newAttrList.append(task)
+        efnDic.update({iD:newAttrList})
+    jsonDic = efnDic
+    with open('todo_all_employees.json'.format(argv[1]), 'w', encoding='utf-8') as jsonFile:
         jString = json.dumps(jsonDic)
         jsonFile.write(jString)
 
-'''
+
 if __name__ == '__main__':
     main()
